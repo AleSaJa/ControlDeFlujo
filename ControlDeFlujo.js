675e993837b0ca1ead9ea028515fad4477cda55d
AleSaJa <alesaja87@gmail.com>
2022-01-16 19:11:10 -0600

    Arreglando errores y ajustando los requerimientos del programa

--- ControlDeFlujo.js
+++ ControlDeFlujo.js
@@ -3,11 +3,12 @@
 
 //Parte 1
 var diaSemana = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sabado"];
-
+var temp1 = temp2 = 0;
 var dia = parseInt(prompt("Porporciona un día de la semana: \nValores admitidos 0 - 6 (Domingo-Sabado)"));
-var hora = parseInt(prompt("Proprorciona una del día: \nValores admitidos 0 - 24"));
+var hora = parseInt(prompt("Proprorciona una hora del día: \nValores admitidos 0 - 24"));
 
-if(isNaN(temp1)||isNaN(temp2)){
+if(isNaN(dia) || isNaN(hora) || dia<0 || dia>6 || hora<0 || hora>24){
+    //Comprueba que los datos ingresados sean correctos
     alert("Datos Invalidos");
 }else{
     alert("Día: "+diaSemana[dia]+"\nHorario: "+hora+"hrs"+"\nEs horario de trabajo: "+businessHours(dia,hora));
@@ -16,73 +17,70 @@ if(isNaN(temp1)||isNaN(temp2)){
 //Función buinessHours
 function businessHours(dayNumber, hourNumber){
     if((dayNumber>=1&&dayNumber<=5)&&(hourNumber>=9&&hourNumber<=18)){
-        return "si";
+        //Comprueba que el día de la semana este entre los intervalos Lunes-Viernes y la hora entre 9-18 hrs
+        return true;
     }else{
-        return "no";
+        return false;
     }
 }
 
 //Parte 2
 
 
-var temp1 = parseInt(prompt("Porporciona un día de la semana, para el Primero de Enero: \nValores admitidos 0 - 6 (Domingo-Sabado)"));
-var temp2 = parseInt(prompt("Proporciona un día del año: \nValores admitidos 0 - 365"));
+temp1 = parseInt(prompt("Porporciona un día de la semana, para el Primero de Enero: \nValores admitidos 0 - 6 (Domingo-Sabado)"));
+temp2 = parseInt(prompt("Proporciona un día del año: \nValores admitidos 0 - 365"));
 
-if(isNaN(temp1)||isNaN(temp2)){
+if( isNaN(temp1) || isNaN(temp2) || temp1<0 || temp1>6 || temp2<0 ||temp2>365 ){
+    //Comprueba que los datos ingresados sean correctos
     alert("Datos Invalidos");
 }else{
-    var temp=getDayNumber(temp1,temp2);
-    alert("El día de  "+temp2+" del calendario, sera: "+diaSemana[temp]);
+    temp1=getDayNumber(temp1,temp2);
+    alert("El día de  "+temp2+" del calendario, fue: "+diaSemana[temp1]);
 }
 
 //Función getDayNumber
 function getDayNumber(janFirstDayNumber,yearDayNumber){
     let temp=0;
-    if(yearDayNumber>1&&yearDayNumber<=6){
-        return janFirstDayNumber+yearDayNumber-1;
-    }else if(yearDayNumber==0||yearDayNumber==1){
-        return janFirstDayNumber;
-    }
-    switch(yearDayNumber%7){
+    switch(yearDayNumber%7){//Ajusta el día de la sema dependiendo del valor janFirstDayNumber
         case 0:
-            temp=janFirstDayNumber-1;
+            temp=janFirstDayNumber-2;
         break;
         case 1:
-            temp=janFirstDayNumber;
+            temp=janFirstDayNumber-1;
         break;
         case 2:
-            temp=janFirstDayNumber+1;
+            temp=janFirstDayNumber;
         break;
         case 3:
-            temp=janFirstDayNumber+2;
+            temp=janFirstDayNumber+1;
         break;
         case 4:
-            temp=janFirstDayNumber+3;
+            temp=janFirstDayNumber-5;
         break;
         case 5:
-            temp=janFirstDayNumber+4;
+            temp=janFirstDayNumber-4;
         break;
         case 6:
-            temp=janFirstDayNumber+5;
+            temp=janFirstDayNumber-3;
         break;
     }
-    switch(temp){
+    switch(temp){//Ajusta el valor del día dependiendo del arreglo diaSemana
+        case -2:
+            temp=5;
+        break;
         case -1:
             temp=6;
         break;
         case 7:
             temp=0;
         break;
-        case 8:
-            temp=1;
-        break;
-        case 9:
+        case -5:
             temp=2;
         break;
-        case 10:
+        case -4:
             temp=3;
         break;
-        case 11:
+        case -3:
             temp=4;
         break;
     }
@@ -92,20 +90,16 @@ function getDayNumber(janFirstDayNumber,yearDayNumber){
 //Parte 3
 
 var temp2 = parseInt(prompt("Calendario del 2022\nProporciona un día del año: \nValores admitidos 0 - 365"));
-var hora = parseInt(prompt("Proprorciona una del día: \nValores admitidos 0 - 24"));
+var hora = parseInt(prompt("Proprorciona una hora del día: \nValores admitidos 0 - 24"));
 
-if(isNaN(hora)||isNaN(temp2)){
+if( isNaN(hora) || isNaN(temp2) || temp2<0 || temp2>365 || hora<0 || hora>24){
+    //Comprueba que los datos ingresados sean correctos
     alert("Datos Invalidos");
 }else{
-    alert("Calendario del 2022\nDía: "+temp2+"\nHorario: "+hora+"hrs"+"\nEs horario de trabajo: "+getDayBusinessHours(hora,temp2));
+    alert("Calendario del 2021\nDía: "+temp2+" "+diaSemana[getDayNumber(0,temp2)]+"\nHorario: "+hora+"hrs"+"\nEs horario de trabajo: "+getDayBusinessHours(hora,temp2));
 }
 
 function getDayBusinessHours(hourNumber,yearDayNumber){
-    let temp=getDayNumber(6,yearDayNumber);
-    if((temp>=1&&temp<=5)&&(hourNumber>=9&&hourNumber<=18)){
-        return "si";
-    }else{
-        return "no";
-    }
+    let temp=getDayNumber(0,yearDayNumber);//Obtiene el día correspondiente a yearDayNumber, tomando como referencia el calendario 2022
+    return businessHours(temp,hourNumber);//Retorna "true" si el día y el horario son laborables; en caso contrario retorna "false"
 }
-
